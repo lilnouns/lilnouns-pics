d0ec3ffb17ac820515b58b1958fe37639e8b9254
Milad Nekofar <milad@nekofar.com>
2022-08-06 07:41:37 +0430

    refactor: add `londrina-solid` font from google as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'londrina-solid': ['"Londrina Solid"', 'cursive'],
+        'sans': ['"Londrina Solid"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }

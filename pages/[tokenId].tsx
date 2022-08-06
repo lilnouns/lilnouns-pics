991d2ed8dcdd52f36cd50c0c436ab5516a10279a
Milad Nekofar <milad@nekofar.com>
2022-08-06 19:46:10 +0430

    feat: add static page generation for lil nouns

--- pages/[tokenId].tsx
+++ pages/[tokenId].tsx
@@ -1,7 +1,46 @@
 import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from "next";
 
-const Noun = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch(
+    'https://api.lilnouns.dev/nouns'
+  );
+
+  const contentRange = response.headers.get('content-range')
+  const nounsCount = contentRange ? contentRange.split('/')[1] : 0;
+
+  let paths = [];
+  for (let i=0; i <= nounsCount; i++) {
+    paths.push({params: {tokenId: `${i}`}})
+  }
+
+  return ({
+    paths,
+    fallback: false, // can also be true or 'blocking'
+  });
+};
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  const tokenId = context.params!.tokenId;
+
+  if (!tokenId) return { notFound: true };
+
+  const response = await fetch(
+    `https://api.lilnouns.dev/nouns/${tokenId}`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+  return ({
+    // Passed to the page component as props
+    props: {noun: data.data.noun},
+  });
+};
+
+const Noun = ({noun}: any) => {
   const router = useRouter()
   const {tokenId} = router.query
 

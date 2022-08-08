22643d7ecc2dc3de1ce3dec1c041d36fcec1fe31
Milad Nekofar <milad@nekofar.com>
2022-08-08 18:16:11 +0430

    refactor: replace switches by list box on token page

--- pages/[tokenId].tsx
+++ pages/[tokenId].tsx
@@ -1,7 +1,8 @@
-import {useEffect, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from "next";
 import Image from "next/image";
-import {RadioGroup, Switch} from "@headlessui/react";
+import {CheckIcon, SelectorIcon} from '@heroicons/react/solid'
+import {Listbox, Switch, Transition} from "@headlessui/react";
 import Head from "next/head";
 
 const mimeOptions = [
@@ -91,81 +92,156 @@ const Noun = ({tokenId}: { tokenId: string }) => {
 
             <h1 className="px-10 py-3">{mime.name} - {size.name} - {back ? 'Normal' : 'Transparent'}</h1>
 
-            <div className="p-10">
-              <a
-                href={image.body}
-                download={`noun-${tokenId}@${image.size}`}
-              >
-                <Image
-                  alt={""}
-                  src={!isLoading ? image.body : "/assets/images/lil-loading-skull.gif"}
-                  width={256}
-                  height={256}
-                  className="border border-neutral-300 rounded-md"
-                />
-              </a>
-            </div>
-
-            <div className="px-10 py-3">
-              <RadioGroup value={mime} onChange={setMime} className="mt-2">
-                <RadioGroup.Label className="sr-only">Choose a memory option</RadioGroup.Label>
-                <div className="grid grid-cols-3 gap-3 sm:grid-cols-6">
-                  {mimeOptions.map((option) => (
-                    <RadioGroup.Option
-                      key={option.name}
-                      value={option}
-                      className={({active, checked}) =>
-                        classNames(
-                          active ? 'ring-2 ring-offset-2 ring-indigo-500' : '',
-                          checked
-                            ? 'bg-zinc-200 border-transparent text-natural-800 opacity-50 hover:bg-zinc-300'
-                            : 'bg-white border-gray-200 text-gray-900 hover:bg-gray-50',
-                          'border border-neutral-300 rounded-md py-3 px-3 flex items-center justify-center text-sm font-medium uppercase sm:flex-1'
-                        )
-                      }
-                    >
-                      <RadioGroup.Label as="p">{option.name}</RadioGroup.Label>
-                    </RadioGroup.Option>
-                  ))}
+            <div className="grid grid-cols-5 gap-6">
+              <div className="col-span-4 sm:col-span-2">
+                <div className="p-5">
+                  <a
+                    href={image.body}
+                    download={`noun-${tokenId}@${image.size}`}
+                  >
+                    <Image
+                      alt={""}
+                      src={!isLoading ? image.body : "/assets/images/lil-loading-skull.gif"}
+                      width={256}
+                      height={256}
+                      className="border border-neutral-300 rounded-md"
+                    />
+                  </a>
                 </div>
-              </RadioGroup>
-            </div>
+              </div>
 
-            <div className="px-10 py-3">
-              <RadioGroup value={size} onChange={setSize} className="mt-2">
-                <RadioGroup.Label className="sr-only">Choose a memory option</RadioGroup.Label>
-                <div className="grid grid-cols-3 gap-3 sm:grid-cols-6">
-                  {sizeOptions.map((option) => (
-                    <RadioGroup.Option
-                      key={option.name}
-                      value={option}
-                      className={({active, checked}) =>
-                        classNames(
-                          active ? 'ring-2 ring-offset-2 ring-indigo-500' : '',
-                          checked
-                            ? 'bg-zinc-200 border-transparent text-natural-800 opacity-50 hover:bg-zinc-300'
-                            : 'bg-white border-gray-200 text-neutral-800 hover:bg-gray-50',
-                          'border border-neutral-300 rounded-md py-3 px-3 flex items-center justify-center text-sm font-medium uppercase sm:flex-1'
-                        )
-                      }
+              <div className="col-span-4 sm:col-span-3">
+                <div className="p-5">
+                  <Listbox value={mime} onChange={setMime}>
+                    {({open}) => (
+                      <>
+                        <Listbox.Label className="block text-sm font-medium text-gray-700">Type</Listbox.Label>
+                        <div className="mt-1 relative">
+                          <Listbox.Button
+                            className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <span className="block truncate">{mime.name}</span>
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                        </span>
+                          </Listbox.Button>
+
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options
+                              className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                              {mimeOptions.map((option) => (
+                                <Listbox.Option
+                                  key={option.name}
+                                  className={({active}) =>
+                                    classNames(
+                                      active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                                      'cursor-default select-none relative py-2 pl-3 pr-9'
+                                    )
+                                  }
+                                  value={option}
+                                >
+                                  {({selected, active}) => (
+                                    <>
+                        <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                          {option.name}
+                        </span>
+
+                                      {selected ? (
+                                        <span
+                                          className={classNames(
+                                            active ? 'text-white' : 'text-indigo-600',
+                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                          )}
+                                        >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true"/>
+                          </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </>
+                    )}
+                  </Listbox>
+
+                  <Listbox value={size} onChange={setSize}>
+                    {({open}) => (
+                      <>
+                        <Listbox.Label className="mt-3 block text-sm font-medium text-gray-700">Size</Listbox.Label>
+                        <div className="mt-1 relative">
+                          <Listbox.Button
+                            className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <span className="block truncate">{size.name}</span>
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                        </span>
+                          </Listbox.Button>
+
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options
+                              className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                              {sizeOptions.map((option) => (
+                                <Listbox.Option
+                                  key={option.name}
+                                  className={({active}) =>
+                                    classNames(
+                                      active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                                      'cursor-default select-none relative py-2 pl-3 pr-9'
+                                    )
+                                  }
+                                  value={option}
+                                >
+                                  {({selected, active}) => (
+                                    <>
+                                  <span
+                                    className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                    {option.name}
+                                  </span>
+
+                                      {selected ? (
+                                        <span
+                                          className={classNames(
+                                            active ? 'text-white' : 'text-indigo-600',
+                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                          )}
+                                        >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true"/>
+                          </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </>
+                    )}
+                  </Listbox>
+
+                  <Switch.Group as="div" className="mt-3 flex items-center">
+                    <Switch
+                      checked={back}
+                      onChange={setBack}
+                      className={classNames(
+                        back ? 'bg-indigo-600' : 'bg-gray-200',
+                        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      )}
                     >
-                      <RadioGroup.Label as="p">{option.name}</RadioGroup.Label>
-                    </RadioGroup.Option>
-                  ))}
-                </div>
-              </RadioGroup>
-            </div>
-
-            <div className="px-10 py-3">
-              <Switch.Group as="div" className="flex items-center">
-                <Switch
-                  checked={back}
-                  onChange={setBack}
-                  className={classNames(
-                    back ? 'bg-indigo-600' : 'bg-gray-200',
-                    'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                  )}
-                >
                   <span
                     aria-hidden="true"
                     className={classNames(
@@ -173,21 +249,22 @@ const Noun = ({tokenId}: { tokenId: string }) => {
                       'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
                     )}
                   />
-                </Switch>
-                <Switch.Label as="span" className="ml-3">
-                  <span className="text-sm font-medium text-gray-900">Background</span>
-                </Switch.Label>
-              </Switch.Group>
-            </div>
+                    </Switch>
+                    <Switch.Label as="span" className="ml-3">
+                      <span className="text-sm font-medium text-gray-900">Background</span>
+                    </Switch.Label>
+                  </Switch.Group>
 
-            <div className="px-10 py-3">
-              <button
-                onClick={() => fetchImage(tokenId, size.value, mime.value, back)}
-                type="submit"
-                className="mt-2 w-full bg-zinc-200 border border-neutral-300 rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-neutral-800 opacity-50 hover:bg-zinc-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-zinc-500"
-              >
-                Generate
-              </button>
+                  <button
+                    disabled={isLoading}
+                    onClick={() => fetchImage(tokenId, size.value, mime.value, back)}
+                    type="submit"
+                    className="mt-2 w-full bg-zinc-200 border border-neutral-300 rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-neutral-800 opacity-50 hover:bg-zinc-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-zinc-500"
+                  >
+                    Generate
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
